d5916e39323dd4930c6bee35e2e770a8e6ff3cb4
estebanperren <pebi44@gmail.com>
2022-08-19 19:05:09 -0300

    fix class to className

--- src/components/cart/ItemListContainer.jsx
+++ src/components/cart/ItemListContainer.jsx
@@ -7,10 +7,10 @@ const ItemListContainer = ( {articulos}) => {
                  <span>Articulos en el carrito:</span>
             {articulos.map((articulo) => (
               <>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
+                <li className="list-group-item d-flex justify-content-between align-items-center">
                   <img src={articulo.img} alt={articulo.nombre} width="25%" />
                   {articulo.nombre}
-                  <span class="badge bg-primary rounded-pill">
+                  <span className="badge bg-primary rounded-pill">
                     {articulo.cantidad}
                   </span>
                 </li>
