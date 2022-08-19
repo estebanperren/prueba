824f8d5063d617dc2b4988ccbf722e30db688167
estebanperren <pebi44@gmail.com>
2022-08-19 19:12:56 -0300

    fix ul missing

--- src/components/cart/ItemListContainer.jsx
+++ src/components/cart/ItemListContainer.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const ItemListContainer = ( {articulos}) => {
-    return (
+const ItemListContainer = ({ articulos }) => {
+  return (
+    <>
+      <span>Articulos en el carrito:</span>
+      {articulos.map((articulo) => (
         <>
+          <ul className="list-group">
+            <li className="list-group-item d-flex justify-content-between align-items-center">
+              <img src={articulo.img} alt={articulo.nombre} width="25%" />
+              {articulo.nombre}
 
-                 <span>Articulos en el carrito:</span>
-            {articulos.map((articulo) => (
-              <>
-                <li className="list-group-item d-flex justify-content-between align-items-center">
-                  <img src={articulo.img} alt={articulo.nombre} width="25%" />
-                  {articulo.nombre}
-                  <span className="badge bg-primary rounded-pill">
-                    {articulo.cantidad}
-                  </span>
-                </li>
-              </>
-            ))}  
+              <span className="badge bg-primary rounded-pill">
+                {articulo.cantidad}
+              </span>
+            </li>
+          </ul>
         </>
-    );
-}
+      ))}
+    </>
+  );
+};
 
 export default ItemListContainer;
